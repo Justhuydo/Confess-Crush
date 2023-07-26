fdef0db7f1a96c6ffc00241a328391f60810232b
Justhuydo <140657659+Justhuydo@users.noreply.github.com>
2023-07-27 06:01:22 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    titleWeb: "HAPPY BIRTHDAY TO YOU",
+    introTitle: 'Đỗ Nguyễn Thu Thủy',
+    introDesc: `Xin Chào :v`,
+    btnIntro: 'Clic herek',
+    title: 'Cảm ơn sự xuất hiện của em trong cuộc đời anh',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Xin mời điền tên người mà Nguyễn Quốc Nam thương yêu ạ',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
